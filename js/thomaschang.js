5dda1f433528b307b441d970e41c0582c9d194a8
thomaschang322 <thomaschang322@gmail.com>
2016-03-06 23:26:54 +0800

    Porfolio
    index

---
+++ js/thomaschang.js
@@ -0,0 +1,39 @@
+﻿/*
+$(document).ready(function () {
+    $('body').scrollspy({ target: ".navbar-fixed-top", offset: 50 });
+});
+*/
+$(document).ready(
+  function () {
+      $("html").niceScroll();
+  }
+);
+
+
+
+
+function collapseNavbar() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+}
+
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
+
+$(function () {
+    $('a.page-scroll').bind('click', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+$('.navbar-nav ul li a').click(function () {
+    if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
+        $('.navbar-toggle:visible').click();
+    }
+});
